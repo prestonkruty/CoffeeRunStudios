90c2680d4633b1bb60b8804ea69dc1deba1e5411
Christian <christian.auman@outlook.com>
2021-09-27 12:24:27 -0400

    Update Web

--- Company Website/coffee-studios/src/index.js
+++ Company Website/coffee-studios/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; 
-import Home from './Home';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home'; 
 
 ReactDOM.render( 
     <Home />,
   document.getElementById('root')
-); 
-reportWebVitals();
+);  
